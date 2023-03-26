5b198f64f697d1dd17f3606d076e39b59e26f355
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-26 15:56:46 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+/*document.addEventListener("DOMContentLoaded", () => {
+ let form = document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    buildToDo(e.target.create-task-description.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+})*/
+
+const main = document.querySelector('#main-content')
+console.log(main)
+
+const taskform = document.querySelector('#create-task-form')
+const tasklist = document.querySelector('#tasks')
+
+taskform.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const newTask = document.querySelector('#new-task-description').value
+
+tasklist.innerHTML += `
+<li> ${newTask}
+<button data-action="delete" data-matt="powerful" data-charlie="downfield">x</button>
+</li>
+` 
+taskform.reset()
+})
+
+tasklist.addEventListener("click", (e) => {
+  console.log(e.target)
+  if (e.target.dataset.action === "delete"){
+    e.target.parentElement.remove()
+  }
+})
